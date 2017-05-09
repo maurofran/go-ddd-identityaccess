211cc814780c47439cfbec632a43c3d16d926475
Mauro Franceschini <mauro.franceschini@gmail.com>
2017-05-09 19:24:16 +0200

    Basic main.go that connects to mongodb.

--- main.go
+++ main.go
@@ -7,13 +7,43 @@ import (
 	"github.com/maurofran/go-ddd-identityaccess/domain/model"
 	"github.com/maurofran/go-ddd-identityaccess/infrastructure/persistence"
 	"github.com/maurofran/go-ddd-identityaccess/resource"
+	"gopkg.in/mgo.v2"
 	"os"
+)
+
+const (
+	envDbUrl  = "MONGODB_URL"
+	envDbName = "MONGODB_NAME"
+)
+
+const (
+	defaultDbUrl  = "mongodb://localhost:27017"
+	defaultDbName = "identityaccess"
 )
 
 func main() {
 	var g inject.Graph
+	var session *mgo.Session
+
+	dbUrl := os.Getenv(envDbUrl)
+	if dbUrl == "" {
+		dbUrl = defaultDbUrl
+	}
+	session, err := mgo.Dial(dbUrl)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+	defer session.Close()
+
+	dbName := os.Getenv(envDbName)
+	if dbName == "" {
+		dbName = defaultDbName
+	}
 
 	g.Provide(
+		// Setup database connection
+		&inject.Object{Value: session.DB(dbName)},
 		// Setup domain
 		&inject.Object{Value: model.TenantRepository{}},
 		&inject.Object{Value: model.TenantProvisioningService{}},
